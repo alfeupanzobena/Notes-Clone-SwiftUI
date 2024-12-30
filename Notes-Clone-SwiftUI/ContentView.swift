e846e7a7ad684125ef3a96623dbb4fa2d1ed9760
alfeupanzobena <alfeupanzobena28@gmail.com>
2024-12-30 12:13:33 +0100

    Initial Commit

---
+++ Notes-Clone-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Notes-Clone-SwiftUI
+//
+//  Created by Alfeu Panzo Bena on 30/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Notes-Clone-SwiftUI/Notes_Clone_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_Clone_SwiftUIApp.swift
+//  Notes-Clone-SwiftUI
+//
+//  Created by Alfeu Panzo Bena on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Clone_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
